b2763ac764a4f5377a93a2fb37d11e6859b5f99b
Bilal Ehsan <bilalehsan933@gmail.com>
2021-04-30 17:42:52 +0100

    Changed to conform more to the PEP 8 style guide

--- app.py
+++ app.py
@@ -4,80 +4,80 @@ import random
 class App:
     def __init__(self, master): # "master" is the main window
         # Frame (like a container) for the buttons
-        topFrame = Frame(master)
-        topFrame.pack()
+        top_frame = Frame(master)
+        top_frame.pack()
 
         # Buttons
-        self.rockBtn = Button(topFrame, text = 'Rock', padx = 25, pady = 15, bg = 'spring green',
-        font = 'system 18', command = self.choseRock)
-        self.rockBtn.grid(row = 0, column = 0, padx = 35, pady = 55)
+        self.rock_btn = Button(top_frame, text = 'Rock', padx = 25, pady = 15, bg = 'spring green',
+        font = 'system 18', command = self.chose_rock)
+        self.rock_btn.grid(row = 0, column = 0, padx = 35, pady = 55)
 
-        self.paperBtn = Button(topFrame, text = 'Paper', padx = 25, pady = 15, bg = 'orchid1',
-        font = 'system 18', command = self.chosePaper)
-        self.paperBtn.grid(row = 0, column = 1, padx = 35, pady = 55)
+        self.paper_btn = Button(top_frame, text = 'Paper', padx = 25, pady = 15, bg = 'orchid1',
+        font = 'system 18', command = self.chose_paper)
+        self.paper_btn.grid(row = 0, column = 1, padx = 35, pady = 55)
 
-        self.scissorsBtn = Button(topFrame, text = 'Scissors', padx = 25, pady = 15, bg = 'navajo white',
-        font = 'system 18', command = self.choseScissors)
-        self.scissorsBtn.grid(row = 0, column = 2, padx = 35, pady = 55)
+        self.scissors_btn = Button(top_frame, text = 'Scissors', padx = 25, pady = 15, bg = 'navajo white',
+        font = 'system 18', command = self.chose_scissors)
+        self.scissors_btn.grid(row = 0, column = 2, padx = 35, pady = 55)
 
         # Second frame for the user and computer results
-        middleFrame = Frame(master)
-        middleFrame.pack()
+        middle_frame = Frame(master)
+        middle_frame.pack()
 
-        self.userChoiceLbl = Label(middleFrame, text = 'User\'s choice was: ?', padx = 35, pady = 35,
+        self.user_choice_lbl = Label(middle_frame, text = 'User\'s choice was: ?', padx = 35, pady = 35,
         font = 'system 16')
-        self.userChoiceLbl.grid(row = 0, column = 0)
+        self.user_choice_lbl.grid(row = 0, column = 0)
 
-        self.computerChoiceLbl = Label(middleFrame, text = 'Computer\'s choice was: ?', padx = 35, pady = 35,
+        self.computer_choice_lbl = Label(middle_frame, text = 'Computer\'s choice was: ?', padx = 35, pady = 35,
         font = 'system 16')
-        self.computerChoiceLbl.grid(row = 0, column = 1)
+        self.computer_choice_lbl.grid(row = 0, column = 1)
 
         # Third frame for the winner
-        bottomFrame = Frame(master)
-        bottomFrame.pack()
+        bottom_frame = Frame(master)
+        bottom_frame.pack()
 
-        self.winner = Label(bottomFrame, text = 'The winner is... 😓', padx = 35, pady = 35,
+        self.winner = Label(bottom_frame, text = 'The winner is... 😓', padx = 35, pady = 35,
         font = 'system 26')
         self.winner.grid(row = 0, columnspan = 3)
 
     # Functions go here
-    def choseRock(self):
+    def chose_rock(self):
         self.play('Rock')
 
-    def chosePaper(self):
+    def chose_paper(self):
         self.play('Paper')
 
-    def choseScissors(self):
+    def chose_scissors(self):
         self.play('Scissors')
 
-    def play(self, userChoice):
+    def play(self, user_choice):
         choices = ['Rock', 'Paper', 'Scissors']
-        computerChoice = random.choice(choices)
-        hasWon = True
+        computer_choice = random.choice(choices)
+        has_won = True
 
         # To update the labels
-        self.userChoiceLbl.config(text = f'User\'s choice was: {userChoice}')
-        self.computerChoiceLbl.config(text = f'Computer\'s choice was: {computerChoice}')
+        self.user_choice_lbl.config(text = f'User\'s choice was: {user_choice}')
+        self.computer_choice_lbl.config(text = f'Computer\'s choice was: {computer_choice}')
 
         # Combinations
-        if userChoice == 'Rock' and computerChoice == 'Paper':
-            hasWon = False
-        elif userChoice == 'Paper' and computerChoice == 'Rock':
-            hasWon = True
-        elif userChoice == 'Paper' and computerChoice == 'Scissors':
-            hasWon = False
-        elif userChoice == 'Scissors' and computerChoice == 'Paper':
-            hasWon = True
-        elif userChoice == 'Rock' and computerChoice == 'Scissors':
-            hasWon = True
-        elif userChoice == 'Scissors' and computerChoice == 'Rock':
-            hasWon = False
-        elif userChoice == computerChoice:
-            hasWon = None
+        if user_choice == 'Rock' and computer_choice == 'Paper':
+            has_won = False
+        elif user_choice == 'Paper' and computer_choice == 'Rock':
+            has_won = True
+        elif user_choice == 'Paper' and computer_choice == 'Scissors':
+            has_won = False
+        elif user_choice == 'Scissors' and computer_choice == 'Paper':
+            has_won = True
+        elif user_choice == 'Rock' and computer_choice == 'Scissors':
+            has_won = True
+        elif user_choice == 'Scissors' and computer_choice == 'Rock':
+            has_won = False
+        elif user_choice == computer_choice:
+            has_won = None
 
-        if hasWon == True:
+        if has_won == True:
             self.winner.config(text = 'The winner is... You! 😍', fg = 'green2')
-        elif hasWon == False:
+        elif has_won == False:
             self.winner.config(text = 'The winner is... Me! 😥', fg = 'indianred1')
         else:
             self.winner.config(text = 'The game was a draw! 😲', fg = 'orange')
